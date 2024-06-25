868fd4efb24503ace4e6d04c06d776eaa66bcb4d
srand <randip.singh@edu.bib.de>
2024-06-25 14:26:18 +0200

    Code

---
+++ Task.java
@@ -0,0 +1,36 @@
+public class Task{
+    private int id;
+    private String description;
+    private boolean completed;
+
+    public Task(int id, String description, boolean completed) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id){
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void complete() {
+        this.completed = true;
+    }
+
+}
---
+++ Taskmanager.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Taskmanager {
+    private Task[] tasks;
+
+    public Taskmanager(int maxTasks) {
+        tasks = new Task[maxTasks];
+    }
+
+    public Task[] getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Task[] tasks) {
+        this.tasks = tasks;
+    }
+    public void add(Task task) {
+        for (int i = 0; i < tasks.length; i++) {
+            if (tasks[i] == null) {
+                tasks[i] = task;
+            }
+        }
+    }
+    public void done(Task task) {
+        task.complete();
+        for (int i = 0; i < tasks.length; i++) {
+            if (tasks[i].equals(task)) {
+                tasks[i] = null;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Taskmanager{" +
+                "tasks=" + Arrays.toString(tasks) +
+                '}';
+    }
+}
